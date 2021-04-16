5861eb7037fea57229860801b6d666e59892e4a7
Qais Patankar <qaisjp@gmail.com>
2021-04-17 00:24:08 +0100

    allow self-horny without throttle

--- horny.go
+++ horny.go
@@ -8,26 +8,30 @@ import (
 )
 
 func horny(s *discordgo.Session, e *discordgo.InteractionCreate) {
-	// check time
-	t, ok := savedData.Horny[e.Member.User.ID]
-	if ok {
-		now := time.Now()
-		allowedAfter := t.Add(time.Hour * 8)
-		if allowedAfter.After(now) {
-			err := s.InteractionRespond(e.Interaction, &discordgo.InteractionResponse{
-				Type: discordgo.InteractionResponseChannelMessageWithSource,
-				Data: &discordgo.InteractionApplicationCommandResponseData{
-					Content: "You can use this again after " + allowedAfter.Sub(now).String(),
-					Flags:   64, // ephemeral
-				},
-			})
-			fmt.Printf("Failed to respond to %#v, error: %v\n", *e.Interaction, err)
-			return
+	target := e.Data.Options[0].StringValue()
+	sourceUserID := e.Member.User.ID
+
+	if sourceUserID != target {
+		// check time
+		t, ok := savedData.Horny[sourceUserID]
+		if ok {
+			now := time.Now()
+			allowedAfter := t.Add(time.Hour * 8)
+			if allowedAfter.After(now) {
+				err := s.InteractionRespond(e.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionApplicationCommandResponseData{
+						Content: "You can use this again after " + allowedAfter.Sub(now).String(),
+						Flags:   64, // ephemeral
+					},
+				})
+				fmt.Printf("Failed to respond to %#v, error: %v\n", *e.Interaction, err)
+				return
+			}
 		}
 	}
 
 	c := "808809400667209820"
-	target := e.Data.Options[0].StringValue()
 	err := s.GuildMemberMove(theGuild, target, &c)
 	if err != nil {
 		err := s.InteractionRespond(e.Interaction, &discordgo.InteractionResponse{
@@ -42,8 +46,10 @@ func horny(s *discordgo.Session, e *discordgo.InteractionCreate) {
 	}
 
 	// update time
-	savedData.Horny[e.Member.User.ID] = time.Now()
-	saveData()
+	if sourceUserID != target {
+		savedData.Horny[e.Member.User.ID] = time.Now()
+		saveData()
+	}
 
 	// push response
 	err = s.InteractionRespond(e.Interaction, &discordgo.InteractionResponse{
